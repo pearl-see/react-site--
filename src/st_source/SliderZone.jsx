d46380a738be1a158eb51071aeda0eda3c6b9feb
pearl <kjj0837@naver.com>
2024-07-25 09:33:36 +0900

    Four-automoveImg

--- src/st_source/SliderZone.jsx
+++ src/st_source/SliderZone.jsx
@@ -14,24 +14,35 @@ export function SliderZone(){
     if (_index<0){_index = 0;}else if(_index>sliders.length-1){_index = sliders.length-1}
   setCurrentIndex(_index);
   }
+  //자동으로 움직여봅시다
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setCurrentIndex((idx)=>(idx+1)% sliders.length)
+  },3000);
+  return()=>clearInterval(interval);
+  },[sliders.length]);
   return(
     <div id="sliderZone">
                 <div id="sub_photo_bg">
                     <ul className="slider_panel"
                     style={{left:`-${currentIndex*100}%`}}>
-                      {/* 슬라이스를 왼쪽으로 100%씩 움직인다 ex) 1번 슬라이스 = 0*100 = 0% 면 2번 슬라이스는 1*100 = 100%가 되어
-                      왼쪽으로 100%만큼 움직임 */}
-                        <li className="slider_image"><img src={slider1} alt=""/></li>
-                        <li className="slider_image"><img src={slider2} alt=""/></li>
-                        <li className="slider_image"><img src={slider3} alt=""/></li>
-                         {/* <li className="slider_image"><img src="img/1.jpg" alt=""></li>
-                        <li className="slider_image"><img src="img/2.jpg" alt=""></li>
-                        <li className="slider_image"><img src="img/3.jpg" alt=""/></li> */}
+                      {/* /* 슬라이스를 왼쪽으로 100%씩 움직인다 ex) 1번 슬라이스 = 0*100 = 0% 면 2번 슬라이스는 1*100 = 100%가 되어
+                      왼쪽으로 100%만큼 움직임 */ 
+                      sliders.map(
+                            (slider,index)=>(
+                              <li key={index} className="slider_image"><img src={slider} alt="" /></li>
+                             ) //이미지를 단순하게 정리, key는 반복할 때 필요함요
+                          )}
+                      
                     </ul>
                     <div className="control_panel">
-                        <div className="control_btn active"  data-index="0"></div>
-                        <div className="control_btn" data-index="1"></div>
-                        <div className="control_btn" data-index="2"></div>
+                       {
+                        sliders.map((slider,index)=>{
+                          return(
+                            <div className={`control_btn ${currentIndex === index? 'active':''}`} onClick={()=>{setCurrentIndex(index);}}/>
+                          )
+                        })
+                       }
                     </div>
                     <div className="direct_btn">
                         <div className="left_btn" onClick={()=>handleArrowClick(-1)}><img src={left_btnImg} alt=""/></div>
